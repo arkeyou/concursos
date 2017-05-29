6344c79b9d663a621619f0199cc002e632a226ba
Erico Gomes <ericogomez@gmail.com>
2017-05-29 14:16:35 -0300

    Inclusão das alterações feitas por mim no curso

--- cursoprodigio-backend/src/main/java/br/gov/prodemge/prodigio/cursoprodigio/negocio/compromisso/CompromissoRN.java
+++ cursoprodigio-backend/src/main/java/br/gov/prodemge/prodigio/cursoprodigio/negocio/compromisso/CompromissoRN.java
@@ -6,37 +6,46 @@ import java.util.Map;
 import br.gov.prodemge.prodigio.cursoprodigio.entidades.compromisso.CompromissoVO;
 import br.gov.prodigio.comuns.anotacoes.RegraDeNegocio;
 import br.gov.prodigio.comuns.anotacoes.enumeracao.MomentoDeExecucao;
+import br.gov.prodigio.comuns.constantes.Constantes;
 import br.gov.prodigio.comuns.excecoes.ViolacaoDeRegraEx;
 import br.gov.prodigio.negocio.ProRN;
 
 public class CompromissoRN extends ProRN {
 
-	@RegraDeNegocio(autor = "Autor", codigo = "CODIGO_DA_REGRA", ordem = 1, fluxo = "gravar", momentoDeExecucao = MomentoDeExecucao.ANTES)
+	@RegraDeNegocio(autor = "Érico Gomes", codigo = "RN_01", ordem = 1, fluxo = Constantes.FLUXO.LISTAR_BASEADO_NO_EXEMPLO, momentoDeExecucao = MomentoDeExecucao.ANTES)
+	public void limpaLista(CompromissoVO compromissoVO) {
+		compromissoVO.getAtributo("listaObjetos");
+	}
+	
+	@RegraDeNegocio(autor = "Érico Gomes", codigo = "RN_01", ordem = 1, fluxo = "gravar", momentoDeExecucao = MomentoDeExecucao.ANTES)
 	public void verificaDescricaoVazia(CompromissoVO compromissoVO) {
-		if (compromissoVO.getDescricao() == null
-				|| compromissoVO.getDescricao().isEmpty()) {
-			throw new ViolacaoDeRegraEx(
-					"O Compromisso precisa de uma descrição.");
+		if (compromissoVO.getDescricao() == null || compromissoVO.getDescricao() .isEmpty())   
+	           {
+			throw new ViolacaoDeRegraEx("O Compromisso precisa de uma descrição.");
 		}
 	}
-
-	@RegraDeNegocio(autor = "Autor", codigo = "CODIGO_DA_REGRA", ordem = 1, fluxo = "gravar", momentoDeExecucao = MomentoDeExecucao.ANTES)
-	public void verificaDataMaior(CompromissoVO compromissoVO) {
-		if (compromissoVO.getDataFim().before(compromissoVO.getDataInicio())) {
-			Map<String, String> erros = new LinkedHashMap<String, String>();
-			erros.put("dataInicio", "A data fim não pode ser menor que a data inicio");
-			erros.put("dataFim", "A data inicio não pode ser maior que a data fim");
-			throw new ViolacaoDeRegraEx(erros);
+	
+	@RegraDeNegocio(autor = "Érico Gomes", codigo = "RN_02", ordem = 2, fluxo = "gravar", momentoDeExecucao = MomentoDeExecucao.ANTES)
+	public void verificaDataFimVazia(CompromissoVO compromissoVO) {
+		if (compromissoVO.getDataFim() == null)   
+	           {
+			throw new ViolacaoDeRegraEx("A data fim do compromisso não pode ser vazia.");
 		}
 	}
-
-	@RegraDeNegocio(autor = "Autor", codigo = "CODIGO_DA_REGRA", ordem = 1, fluxo = "gravar", momentoDeExecucao = MomentoDeExecucao.ANTES)
-	public void verificaDataMenor(CompromissoVO compromissoVO) {
+	
+	@RegraDeNegocio(autor = "Érico Gomes", codigo = "RN_03", ordem = 3, fluxo = "gravar", momentoDeExecucao = MomentoDeExecucao.ANTES)
+	public void verificaDataInicioVazia(CompromissoVO compromissoVO) {
+		if (compromissoVO.getDataInicio() == null) {
+			Map<String, String> mapaErros = new LinkedHashMap<String, String>();
+			mapaErros.put("dataInicio", "A data inicio do compromisso não pode ser vazia.");
+			throw new ViolacaoDeRegraEx(mapaErros);
+		}
+	}
+	
+	@RegraDeNegocio(autor = "Érico Gomes", codigo = "RN_04", ordem = 4, fluxo = "gravar", momentoDeExecucao = MomentoDeExecucao.ANTES)
+	public void verificaDataInicioAposDataFim(CompromissoVO compromissoVO) {
 		if (compromissoVO.getDataInicio().after(compromissoVO.getDataFim())) {
-			Map<String, String> erros = new LinkedHashMap<String, String>();
-			erros.put("dataInicio", "A data fim não pode ser menor que a data inicio");
-			erros.put("dataFim", "A data inicio não pode ser maior que a data fim");
-			throw new ViolacaoDeRegraEx(erros);
+			throw new ViolacaoDeRegraEx("A data do compromisso deve ser antes da data fim.");
 		}
 	}
 }
